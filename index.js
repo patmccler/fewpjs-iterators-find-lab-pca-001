32df0a7397a95ea29f10688b120319ac5ac44a10
Pat McClernan <patmccler@gmail.com>
2020-07-16 16:08:31 -0400

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arr) {
+  let found =  arr.find((obj) => (obj.result === "W") ? true : false)
+  return (found ? found.year : undefined)
+}
